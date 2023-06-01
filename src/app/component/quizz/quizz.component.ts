dc3118ac4ec965f01de7ad21c74aec7f6a098b81
Roberto <robertosilva.2@hotmail.com>
2023-05-31 22:05:18 -0300

    criando regra do jogo

--- src/app/component/quizz/quizz.component.ts
+++ src/app/component/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quizz',
@@ -24,8 +25,29 @@ export class QuizzComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
 
-    
+      this.questions = quizz_questions.questions
+      this.questionsSelected = this.questions[this.questionIndex]
+      this.questionIndex =0
+      this.questionMaxIndex = this.questions.length
+    }
+
   }
 
+  playerChoose(value:string){
+    this.answers.push(value)
+
+  }
+  async nexStep(){
+    this.questionIndex+= 1
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionsSelected = this.questions[this.questionIndex]
+    }else{
+      this.finished =true
+    }
+  }
 }
